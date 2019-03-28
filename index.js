e46a889660bc12cc36395c3489a7cb1816a4f708
sukhjit2008 <sukhit2008@gmail.com>
2019-03-28 13:49:17 -0600

    added arr

--- index.js
+++ index.js
@@ -14,90 +14,91 @@
       this.newarr = [];
       this.number = [];
     }
-  // Print no between 1 and 99
+  // Print no between 1 to 99 inclusive
     oneToHundred(n) {
+      const arr=[' one',' two',' three',' four',' five',' six',' seven',' eight',' nine',' ten',' eleven',' twelve',' thirteen',' fourteen',' fifteen',' sixteen',' seventeen',' eighteen',' nineteen',' twenty',' thirty',' fourty',' fifty',' sixty',' seventy',' eighty',' ninety'];
       let str = "";
       while (n != 0) {
         if (n === 1) {
-          str += ` one`;
+          str += arr[0];
           n = 0;
         } else if (n === 2) {
-          str += ` two`;
+          str += arr[1];
           n = 0;
         } else if (n === 3) {
-          str += ` three`;
+          str += arr[2];
           n = 0;
         } else if (n === 4) {
-          str += ` four`;
+          str += arr[3];
           n = 0;
         } else if (n === 5) {
-          str += ` five`;
+          str += arr[4];
           n = 0;
         } else if (n === 6) {
-          str += ` six`;
+          str += arr[5];
           n = 0;
         } else if (n === 7) {
-          str += ` seven`;
+          str += arr[6];
           n = 0;
         } else if (n === 8) {
-          str += ` eight`;
+          str += arr[7];
           n = 0;
         } else if (n === 9) {
-          str += ` nine`;
+          str += arr[8];
           n = 0;
         } else if (n === 10) {
-          str += ` ten`;
+          str += arr[9];
           n = 0;
         } else if (n === 11) {
-          str += ` eleven`;
+          str += arr[10];
           n = 0;
         } else if (n === 12) {
-          str += ` tweleve`;
+          str += arr[11];
           n = 0;
         } else if (n === 13) {
-          str += ` thirdeen`;
+          str += arr[12];
           n = 0;
         } else if (n === 14) {
-          str += ` fourteen`;
+          str += arr[13];
           n = 0;
         } else if (n === 15) {
-          str += ` fifteen`;
+          str += arr[14];
           n = 0;
         } else if (n === 16) {
-          str += ` sixteen`;
+          str += arr[15];
           n = 0;
         } else if (n === 17) {
-          str += ` seventeen`;
+          str += arr[16];
           n = 0;
         } else if (n === 18) {
-          str += ` eighteen`;
+          str += arr[17];
           n = 0;
         } else if (n === 19) {
-          str += ` nineteen`;
+          str += arr[18];
           n = 0;
         } else if (n >= 20 && n < 30) {
-          str += ` twenty`;
+          str += arr[19];
           n -= 20;
         } else if (n >= 30 && n < 40) {
-          str += ` thirty`;
+          str += arr[20];
           n -= 30;
         } else if (n >= 40 && n < 50) {
-          str += ` fourty`;
+          str += arr[21];
           n -= 40;
         } else if (n >= 50 && n < 60) {
-          str += ` fifty`;
+          str += arr[22];
           n -= 50;
         } else if (n >= 60 && n < 70) {
-          str += ` sixty`;
+          str += arr[23];
           n -= 60;
         } else if (n >= 70 && n < 80) {
-          str += ` seventy`;
+          str += arr[24];
           n -= 70;
         } else if (n >= 80 && n < 90) {
-          str += `eighty`;
+          str += arr[25];
           n -= 80;
         } else if (n >= 90 && n < 100) {
-          str += ` ninty`;
+          str += arr[26];
           n -= 90;
         }
       }
@@ -123,7 +124,7 @@
       return str;
     }
     range(a, b) {
-      //Condition checks if the no entered in between 1 and 999 inclusive
+      //Condition checks if numbers entered are between 1 and 999 inclusive and  if second no is greater than first no.
       if (a > 0 && a < 10000 && b > 0 && b < 10000 && b > a) {
         for (let i = a; i <= b; i++) {
           const s = this.findRange(i);
